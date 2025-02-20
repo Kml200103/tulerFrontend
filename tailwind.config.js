ef912ef4973d208af5e9e25cfc4644aea5c1d81f
Kamal <kml200103@gmail.com>
2025-02-20 15:03:19 +0530

    minor
    minor

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
 
       padding: "1.5rem", // Adjust left/right padding
     },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      mid: "912px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
